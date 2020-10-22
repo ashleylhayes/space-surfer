01213cf34a849279bf4e404b587637aef7298f3d
Ashley Hayes <ashley_hayes@hotmail.ca>
2020-10-21 22:59:57 -0600

    completed more work on games

--- client/src/components/CardGame/CardGame.jsx
+++ client/src/components/CardGame/CardGame.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../CardGame/CardGame.scss';
 import alienImage from '../../assets/memory-alien.png';
 import astronautImage from '../../assets/memory-astronaut.png';
@@ -8,62 +8,64 @@ import spaceshipImage from '../../assets/memory-spaceship.png';
 import starImage from '../../assets/memory-star.png';
 import cardLogo from '../../assets/memory-logo.png';
 
-class CardGame extends React.Component {
-    
+class CardGame extends Component {
+    state = { isActive: false };
+
     flipCard = () => {
         console.log('I was clicked!');
         console.log(this);
+        this.setState({ isActive: !this.state.isActive });
     }
 
     render() {
 
         return (
             <div className='card-game'>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={alienImage} alt='alien' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={alienImage} alt='alien' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={astronautImage} alt='astronaut' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={astronautImage} alt='astronaut' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={planetImage} alt='planet' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={planetImage} alt='planet' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={rocketImage} alt='rocket' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={rocketImage} alt='rocket' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={spaceshipImage} alt='spaceship' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={spaceshipImage} alt='spaceship' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={starImage} alt='star' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
-                <div onClick={this.flipCard} className='card-game__matching-card'>
+                <div onClick={this.flipCard} className={this.state.isActive ? 'card-game__matching-card flip' : 'card-game__matching-card'}>
                     <img className='card-game__matching-card-front' src={starImage} alt='star' />
                     <img className='card-game__matching-card-back' src={cardLogo} alt='space surfer logo' />
                 </div>
